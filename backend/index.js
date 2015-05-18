8f3df87d3066582078c2ab3593959e202e96309f
Corentin Cadiou <corentin.cadiou@linagora.com>
2015-05-18 17:48:53 +0200

    MEET-337 Inject CSS and JS

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,22 @@ var AwesomeYjsModule = new AwesomeModule('linagora.esn.text-editor', {
       return callback(null, {});
     },
     deploy: function (dependencies, callback) {
-
       // register the webapp
       var app = require('./webserver/application')(dependencies);
       var webserver = dependencies('webserver-wrapper');
+
+      webserver.injectAngularModules('yjs', ['app.js'], 'yjsEditor', ['live-conference']);
       webserver.injectCSS('yjs', ['editor.css'], ['live-conference']);
+
+      // Inject extra dependencies
+      var depList = {
+        js: ["../quill/dist/quill.js", "yjs/y.js", "y-list/y-list.js",
+          "y-selections/y-selections.js", "y-richtext/y-richtext.js"],
+        css: ["quill/dist/quill.snow.css", "editor.css"]
+      };
+      webserver.injectJS('third_party', depList.js, ['live-conference'])
+      webserver.injectCSS('third_party', depList.js, ['live-conference']);
+
       webserver.addApp('yjs', app);
       return callback(null, {});
     }
